6601bf47aa4fcdd4d4e843d7e27fe5bce6217d39
Amanuel Chukala <amanuelchukala@gmail.com>
2023-09-05 11:55:15 -0600

    Update

--- main.py
+++ main.py
@@ -4,45 +4,62 @@ import pandas as pd
 
 BACKGROUND_COLOR = "#B1DDC6"
 data_size = 0
+current_word = ""
+french_word = ""
+english_word = ""
+vocabulary = None
 
 
 # ---------------------------- FLIP CARD --------------------------- #
-def flip_card(canvas, card_img, new_img, word_label, english_word, title_label):
+def flip_card(canvas, card_img, new_img, word_label, english, title_label):
     canvas.itemconfig(card_img, image=new_img)
     canvas.itemconfig(title_label, text="English", fill="white")
-    canvas.itemconfig(word_label, text=english_word, fill="white")
+    canvas.itemconfig(word_label, text=english, fill="white")
 
 
 # ---------------------------- GENERATE WORDS -----------------------  #
 def generate_words(window, data, canvas, word_label, card_img, back, title_label, front, remove):
-    french_word = random.choice(list(data.keys()))
-    english_word = data[french_word]
-    canvas.itemconfig(card_img, image=front)
-    canvas.itemconfig(title_label, text="French", fill="black")
-    canvas.itemconfig(word_label, text=french_word, fill="black")
-    timer = window.after(3000, flip_card, window, canvas, card_img, back, word_label, english_word, title_label)
-    print(len(data))
-    if remove:
-        data.__delitem__(french_word)
-        pd.DataFrame.from_dict(data, orient="index").to_csv("./data/words_to_learn.csv")
+    global current_word
+    global french_word
+    global english_word
+
+    if data:
+        current_word = french_word
+        french_word = random.choice(list(data.keys()))
+        english_word = data[french_word]
+        canvas.itemconfig(card_img, image=front)
+        canvas.itemconfig(title_label, text="French", fill="black")
+        canvas.itemconfig(word_label, text=french_word, fill="black")
+
+        if remove and data[current_word]:
+            data.__delitem__(current_word)
+            current_word = french_word
+            pd.DataFrame.from_dict(data, orient="index").to_csv("./data/words_to_learn.csv")
+        window.after(3000, flip_card, canvas, card_img, back, word_label, english_word, title_label)
+    else:
+        # os.remove("./data/words_to_learn.csv")
+        canvas.itemconfig(title_label, text="Congratulations! 🥳🎉\nYou've learned every word.")
+        canvas.itemconfig(word_label, text="")
+        # window.after
 
 
 # ---------------------------- READ FILE ----------------------------- #
 def read_file():
     try:
         data = pd.read_csv("./data/words_to_learn.csv")
-        print("Reading from words_to_learn")
+        if data.empty:
+            data = pd.read_csv("./data/french_words.csv")
     except FileNotFoundError:
         data = pd.read_csv("./data/french_words.csv")
-        print("Reading from french_words")
     dict_data = dict(data.values)
     return dict_data
 
 
 # ---------------------------- UI SETUP ------------------------------- #
 def main():
-    vocabulary = read_file()
+    global vocabulary
     global data_size
+    vocabulary = read_file()
     data_size = len(vocabulary)
     # remove = False
     window = Tk()
@@ -70,7 +87,8 @@ def main():
                                                       title_label, front_img, True))
     right_btn.grid(row=1, column=1)
 
-    generate_words(window, vocabulary, canvas, word_label, card_img, back_img, title_label, front_img, False)
+    generate_words(window, vocabulary, canvas, word_label, card_img, back_img, title_label, front_img,
+                   False)
 
     window.mainloop()
 
